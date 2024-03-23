1ef98f390a1730ecc3d65ad5ba0d2126ed28b04a
AjinkyaVeer007 <veerajinkya1@gmail.com>
2024-03-23 19:51:28 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Appreciation from "./pages/Appreciation";
-import LandingPage from "./pages/LandingPage";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <LandingPage />,
-    },
-    {
-      path: "/appreciation/:userId",
       element: <Appreciation />,
     },
   ]);
